47dfcd7c08c0880851dfdc010fd600db2a78d1f4
Oleg Kovalev <kovalev.o.d@list.ru>
2023-07-25 18:34:45 +0300

    Commit 1, initial

---
+++ m16.HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m16.HW1
+//
+//  Created by Олег Ковалев on 25.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
